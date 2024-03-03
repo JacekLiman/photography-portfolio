5e3360c2060328b6af77d7188fb424ed7f0d86e9
Jacek <limaniec@gmail.com>
2024-03-03 13:19:17 +0100

    add Home page

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,5 +1,43 @@
+import {
+  ImFacebook,
+  ImTwitter,
+  ImPinterest,
+  ImInstagram,
+  ImYoutube,
+} from "react-icons/im";
+
 const Socials = () => {
-  return <section>Socials</section>;
+  return (
+    <div>
+      <ul className="flex items-center gap-x-4">
+        <li>
+          <a href="#">
+            <ImFacebook />
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <ImTwitter />
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <ImPinterest />
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <ImInstagram />
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <ImYoutube />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Socials;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,31 @@
+import WomanImg from "../assets/img/home/woman.png";
+import { Link } from "react-router-dom";
+
 const Home = () => {
-  return <section className="section">jhkjurrg</section>;
+  return (
+    <section className="section">
+      <div className="container mx-auto h-full">
+        <div className="h-full grid grid-cols-1 lg:grid-cols-2 gap-10">
+          <div className="text-center lg:text-left">
+            <h1 className="h1 pt-36 pb-14 lg:text-[]">
+              Photographer <br /> & film maker
+            </h1>
+            <p className="font-primary text-[26px] lg:text-[36px] mb-4 lg:mb-12">
+              Los Angeles, USA
+            </p>
+            <Link className="btn inline-block mb-[30px]" to={"/contact"}>
+              hire me
+            </Link>
+          </div>
+          <div >
+            <div className="h-full">
+              <img className="h-full object-cover" src={WomanImg} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
